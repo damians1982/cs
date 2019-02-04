c4d162078bafd99dbd5fea049502aed43d5fd43f
damian <poczta@damianstasiak.pl>
2019-02-04 09:28:20 +0100

    Added validations and test for uniqueness of property name

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,4 @@
 class Property < ApplicationRecord
   validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- test/models/property_test.rb
+++ test/models/property_test.rb
@@ -5,6 +5,7 @@ class PropertyTest < ActiveSupport::TestCase
     @property1 = Property.new(name: "Kamienica przy Kościuszki 31",regnum: "19",info: "Moja kamienica")
     @property2 = Property.new
   end
+
   test "should be valid" do
      assert @property1.valid?
   end
@@ -17,5 +18,18 @@ class PropertyTest < ActiveSupport::TestCase
     @tmp = Property.new()
     @tmp.name = "    "
     assert_not @tmp.valid?
+  end
+
+  test "cannot save two objects with same name, name must be unique" do
+    @tmp1 = Property.new
+    @tmp1.name = 'name1'
+    @result1 = @tmp1.save
+
+    @tmp2 = Property.new
+    @tmp2.name = 'name1'
+    @result2 = @tmp2.save
+
+    assert_not_equal(@result1,@result2)  
+
   end
 end
