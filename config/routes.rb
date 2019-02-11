1fb882815ffb4dea5f8cc3f8c08a255e57288f49
damian <poczta@damianstasiak.pl>
2019-02-11 16:39:30 +0100

    Added button 'back' from the view Properties.save. Button leads to properties.list

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'properties/edit'
   get 'properties/delete'
   get 'properties/list'
+  post 'properties/list'
   get 'properties/save'
   post 'properties/save'
 
