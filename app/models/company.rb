df3ff6c79d6120941e2cc9f635dcc624b008d0e5
damian <poczta@damianstasiak.pl>
2019-05-19 19:10:04 +0200

    added validation for postal code like xx-xxx

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,8 @@
 class Company < ApplicationRecord
   validates :name,:street,:city,:postal_code,:nip, presence: true
   validates :nip, uniqueness: true
+
+  VALID_POSTAL_CODE_REGEX = /\d\d-\d\d\d/
+
+  validates :postal_code, format: { with: VALID_POSTAL_CODE_REGEX }
 end
