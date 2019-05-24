9311c03f263e3a7f789b11798aab301fcd42d56b
damian <poczta@damianstasiak.pl>
2019-05-24 21:02:15 +0200

    added tests for model (actually postal_code) and validator for that postal code

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,7 @@ class Company < ApplicationRecord
   validates :name,:street,:city,:postal_code,:nip, presence: true
   validates :nip, uniqueness: true
 
-  VALID_POSTAL_CODE_REGEX = /\d\d-\d\d\d/
+  VALID_POSTAL_CODE_REGEX = /\A\d\d-\d\d\d\z/
   validates :postal_code, format: { with: VALID_POSTAL_CODE_REGEX }
 
   #VALID_NIP_OR_PESEL = /(\d\d\d\d\d\d\d\d\d\d)|(\d\d\d\d\d\d\d\d\d\d\d)/
--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -2,10 +2,61 @@ require 'test_helper'
 
 class CompanyTest < ActiveSupport::TestCase
   def setup
-    @company = Company.new(name: "Damianno", street: "krucza" , city: "łódź", postal_code: "99-100", nip: "00-000")
+    @company = Company.new(name: "Damianno", street: "krucza" , city: "łódź",
+      postal_code: "199-100",nip: "00-000")
   end
 
-  test "should be valid" do
-    assert @company.valid?
+  #postal_code value is incorrect and we have assert(NOT)
+  #so the whole test will be true when valid? returns false
+  test "postal code should be not valid - no1" do
+    @company.postal_code = "a99-100"
+      assert_not @company.valid?
   end
+
+  test "postal code should be not valid - no2" do
+    @company.postal_code = "99-100."
+    assert_not @company.valid?
+  end
+
+  test "postal code should be not valid - no3" do
+    @company.postal_code = "99-100-11-200"
+    assert_not @company.valid?
+  end
+
+  test "postal code should be not valid - no4" do
+    @company.postal_code = "99-1000"
+    assert_not @company.valid?
+  end
+
+  test "postal code should be not valid - no5" do
+    @company.postal_code = "000-000"
+    assert_not @company.valid?
+  end
+
+  test "postal code should be not valid - no6" do
+    @company.postal_code = "0000-000"
+    assert_not @company.valid?
+  end
+
 end
+
+  #test "nip should not be valid" do
+  #  puts "putsuje"
+  #  #characters appended at the end and beginning
+  #  @company.nip ="a507-004-84-92v"
+  #  assert_not @company.valid?
+  #end
+
+  #test "nip should not be valid2" do
+  #  puts "putsuje"
+  #  #characters appended at the end
+  #  @company.nip ="507-004-84-92a"
+  #  assert_not @company.valid?
+  #end
+
+  #test "nip should not be valid3" do
+  #  puts "putsuje"
+  #  #characters appended at the end
+  #  @company.nip ="507-004-84-92."
+  #  assert_not @company.valid?
+  #end
